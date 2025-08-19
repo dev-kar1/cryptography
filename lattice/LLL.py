721e061bdc949c293bc1543b04826d9eadc41b3e
dev-kar1 <151200211+dev-kar1@users.noreply.github.com>
2025-08-19 11:00:47 +0530

    Update LLL.py

--- lattice/LLL.py
+++ lattice/LLL.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-'''Gram-Schmidt Orthogonalisation''' 
+'''Gram-Schmidt Orthogonalisation Block''' 
 
 def GramSchmidtCoefficient(b1,b2):
     return np.dot(b1,b2)/np.dot(b2,b2)
@@ -12,39 +12,46 @@ def GramSchmidt(B):
     n = B.shape[0]
     m = B.shape[1]
     GSO = np.zeros((n,m))
-    for i in range (n):
+    for i in range (1,n):
         vector = B[i].copy()
         for j in range (i):
             vector = vector - Projection(vector, GSO[j])
         GSO[i] = vector
     return GSO
-
+"GSO is fine and good to be used in LLL"
 
 '''LLL Implementation'''
-#the lll computation goes here
-def LLL(B):
-    SizeReduce(B) #we will make the function in the dependencies section
-    for i in range (len(B)): #n=len(B) is rank of lattice, no of rows in lattice
-        if(lovasz(bi,bj)=False):
-            bi,bi+1 = b1+1,bi #swapping works like a,b = b,a in python
-            LLL(B)
-        else:
-            return B
+
 #the dependencies of lll go here, namely lovasz condition, sizereduce, etc
-def Lovasz(b1,b2):
-    delta = 0.75 #can change between 0.25 to 1
-    if (delta * (np.dot(b1,b1)) > np.dot((GramSchmidtCoefficient(b2,b1)* b1+b2),(GramSchmidtCoefficient(b2,b1)* b1+b2)):
-        return True
-    else :
-    return False
+def Lovasz(B, GSO, k,  delta = 0.75):
+    mu = GramSchmidtCoefficient(B[k],GSO[k-1])
+    lhs = delta * np.dot(GSO[k-1],GSO[k-1])
+    rhs = np.dot(mu * GSO[k-1] + GSO[k] , mu * GSO[k-1] + GSO[k])
+    return lhs <= rhs
     
     
     
 def SizeReduce(B):
-    GramSchmidt(B)
-    for j in range (2,n):
-        for i in range (j-1,1):
-            m = GramSchmidtCoefficient(b1,b2)
-            bj = bj - round(m)*bi
+    n = B.shape[0]
+    for i in range (1,n):
+        for j in range (i-1,-1,-1):
+            mu = np.round(GramSchmidtCoefficient(B[i],B[j]))
+            if mu != 0:
+                B[i] = B[i] - mu * B[j]
     return B
-    
+
+
+#the LLL computation goes here
+def LLL(B, delta = 0.75):
+    n = B.shape[0]
+    B = B.copy()
+    k = 1
+    SizeReduce(B) #we will make the function in the dependencies section
+    for i in range (len(B)): #n=len(B) is rank of lattice, no of rows in lattice
+        if not Lovasz(B, GSO, k, delta):
+            B[i , i-1] = B[i-1 , i] #swapping works like a,b = b,a in python
+            LLL(B)
+        else:
+            return B
+
+
